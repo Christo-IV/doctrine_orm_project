8eba1407c79156ba3630f8a6b0901256f0e04d67
christo.vahtramae <christo.vahtramae@khk.ee>
2021-02-15 10:44:12 +0200

    Lisatud artiklite autorid

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,6 +10,7 @@ use Slim\Exception\HttpNotFoundException;
 
 class AdminController extends Controller
 {
+     
     public function view(Request $request, Response $response)
     {
         $articles = $this->ci->get('db')->getRepository('App\Entity\Article')->findBy([], [
@@ -20,6 +21,24 @@ class AdminController extends Controller
             'articles' => $articles
         ]);
     }
+/*
+     public function create(Request $request, Response $response, $args = [])
+    {
+        $article = $this->ci->get('db')->find('App\Entity\Article', $args['id']);
+
+        if ($request->isPost()) {
+            $article->setName($request->getParam('name'));
+            $article->setSlug($request->getParam('slug'));
+            $article->setImage($request->getParam('image'));
+            $article->setBody($request->getParam('body'));
+        }
+
+        $this->ci->get('db')->persist($article);
+        $this->ci->get('db')->flush();
+
+        return $this->renderPage($response, 'admin/edit.html', [
+            'article' => $article
+        ]);*/
 
     public function edit(Request $request, Response $response, $args = [])
     {
@@ -34,13 +53,35 @@ class AdminController extends Controller
             $article->setSlug($request->getParam('slug'));
             $article->setImage($request->getParam('image'));
             $article->setBody($request->getParam('body'));
+            $article->setAuthor(
+                $this->ci->get('db')->find('App\Entity\Author', $request->getParam('author'))
+            );
         }
 
         $this->ci->get('db')->persist($article);
         $this->ci->get('db')->flush();
 
-        return $this->renderPage($response, 'admin/edit.html', [
-            'article' => $article
+        $authors = $this->ci->get('db')->getRepository('App\Entity\Author')->findBy([],[
+            'name' => 'ASC'
         ]);
+
+        return $this->renderPage($response, 'admin/edit.html', [
+            'article' => $article,
+            'authors' => $this->authorDropdown($authors, $article)
+        ]);
+    }
+
+    private function authorDropdown($authors, $article) {
+        $options = [];
+
+        foreach ($authors as $author) {
+            $options[] = sprintf(
+                '<option value="%s" "%s">%s</option>',
+                $author->getId(),
+                ($author == $article->getAuthor()) ? 'selected' : '',
+                $author->getName()
+            );
+        }
+        return implode($options);
     }
 }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -34,6 +34,11 @@ class Article {
 	* @Column(type="datetime")
 	*/
 	private $published;
+	/**
+	* @ManyToOne(targetEntity="Author", inversedBy="articles")
+	*/
+	private $author;
+
 
 	public function getId(){
 		return $this->id;
@@ -83,8 +88,11 @@ class Article {
 		$this->published = $value;
 	}
 
+	public function getAuthor(){
+		return $this->author;
+	}
 
-
-
-
+	public function setAuthor($value){
+		$this->author = $value;
+	}
 }
