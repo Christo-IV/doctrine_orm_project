3cb61918c1db0b1963a41fbc482aacacf9e29339
christo.vahtramae <christo.vahtramae@khk.ee>
2021-03-03 15:09:16 +0200

    Lisatud route'ingud siltide haldamise jaoks

--- public/index.php
+++ public/index.php
@@ -25,10 +25,18 @@ $app = AppFactory::create();
 
 // define page routes
 $app->get('/', '\App\Controller\DefaultController:homepage');
+
+// Admin - Articles
 $app->get('/admin/article', '\App\Controller\ArticleAdminController:view');
 $app->any('/admin/article/create', '\App\Controller\ArticleAdminController:create');
 $app->any('/admin/article/{id}', '\App\Controller\ArticleAdminController:edit');
-//    get?
+
+// Admin - Tags
+$app->get('/admin/tag', '\App\Controller\TagAdminController:view');
+$app->any('/admin/tag/create', '\App\Controller\TagAdminController:create');
+$app->any('/admin/tag/{id}', '\App\Controller\TagAdminController:edit');
+
+// Normal view
 $app->any('/article/{slug}', '\App\Controller\ArticleController:view');
 $app->get('/author/{id}', '\App\Controller\AuthorController:author');
 $app->get('/tags', '\App\Controller\TagController:tags');
